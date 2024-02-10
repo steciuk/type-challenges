31e532fd757269fb9ea2c0e28aec6de1eb66bab4
Adam Steciuk <a.t.steciuk@gmail.com>
2024-02-10 20:34:46 +0100

    00007-easy-readonly

--- playground/easy/00007-easy-readonly.ts
+++ playground/easy/00007-easy-readonly.ts
@@ -31,7 +31,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly<T> = any
+type MyReadonly<T> = {readonly [key in keyof T]: T[key]}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
