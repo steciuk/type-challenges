878f515a0715a179abdd9c289de0c1351f3fd085
Adam Steciuk <a.t.steciuk@gmail.com>
2024-02-22 15:32:59 +0100

    00191-medium-append-argument

--- playground/medium/00191-medium-append-argument.ts
+++ playground/medium/00191-medium-append-argument.ts
@@ -23,7 +23,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends Function, A> = Fn extends (...args: infer Args) => infer Return ? (...args: [...Args, A]) => Return : never
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
