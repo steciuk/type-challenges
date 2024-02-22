e31b7f08998e1681c07650d7ecd8ad30438176c4
Adam Steciuk <a.t.steciuk@gmail.com>
2024-02-22 15:07:11 +0100

    00108-medium-trim

--- playground/medium/00108-medium-trim.ts
+++ playground/medium/00108-medium-trim.ts
@@ -18,7 +18,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Trim<S extends string> = any
+type TrimLeft<S extends string> = S extends `${' ' | '\t' | '\n'}${infer R}` ? TrimLeft<R> : S
+type TrimRight<S extends string> = S extends `${infer R}${' ' | '\t' | '\n'}` ? TrimRight<R> : S
+
+type Trim<S extends string> = TrimRight<TrimLeft<S>>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
